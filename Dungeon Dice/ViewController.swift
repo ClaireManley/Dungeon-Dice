6efd7afb1c84bb233ec9d54508d74bbbb3bb6ae6
Claire Manley <cmanley2@zagmail.gonzaga.edu>
2020-02-05 22:41:43 -0800

    Attempt at Dungeon Dice App 2/5

--- Dungeon Dice/ViewController.swift
+++ Dungeon Dice/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func diceButtonPressed(_ sender: UIButton) {
+        let diceRoll = Int.random(in: 1...sender.tag)
+        resultLabel.text = "You Rolled A\ (diceroll) on a \ (sender.tag)-sided dice"
+        
+        
+        
+        var diceRoll: Int
+        var numberOfRolls = 0
+        
+        repeat {
+            diceRoll = Int.random(in: 1....6)
+            numberOfRolls += 1
+            print("Roll #:\ (numberOfRolls) was a\(diceRoll)")
+        }
+        
+        
+        
+    }
+    func numberOfTimesToRoll(number: Int, diceSides: Int) {
+    var diceRoll: Int
+    var numberOfRolls = 0
+    
+    if number <= diceSides {
+    repeat {
+        diceRoll = Int.random(in: 1...diceSides)
+        numberOfRolls += 1
+        print("Roll #:",\(numberOfRolls) was a \(diceRoll)")
+    } while diceRoll != number
+        print("It took \(numberOfRolls) dice rolls to roll a \(number) on a \(diceSides)-sided dice!")
+        <#code#>
+        }else
+    }
+print("Hey! You can't roll a \(number) on a \(diceSides)-sided dice!")
 }
 
+numberOfTimesToRoll(number: 6, diceSides: 10)
+numberOfTimesToRoll(number: 2, diceSides: 4)
+numberOfTimesToRoll(number: 15, diceSides: 20)
+numberOfTimesToRoll(numberL 10, diceSides: 6)
